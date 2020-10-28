0f1cd63bf08f49fbbe9ea1ba6135dfd269bed00d
eternale88 <ethanrobbins@ymail.com>
2020-10-27 20:32:29 -0400

    updat

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack()
+    this.stack2 = new Stack()
+  }
+
+  add(record) {
+    this.stack1.push(record)
+  }
+
+  remove() {
+    //as long as still value in stack1, pop out record and push this popped record onto stack2
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop())
+    }
+    //pop the record off, but don't return yet, first return other records to first stack, in case other operation needed
+    const record = this.stack2.pop()
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop())
+    }
+
+    return record
+  }
+
+  peek() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop())
+    }
+
+    const record = this.stack2.peek()
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop())
+    }
+    return record
+  }
+}
 
 module.exports = Queue;
