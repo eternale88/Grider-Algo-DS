650e144462123028c930e622d92240d00c79439a
eternale88 <ethanrobbins@ymail.com>
2020-11-10 20:55:28 -0500

    add extra linkedlist methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -17,6 +17,7 @@ class LinkedList {
   insertFirst(data) {
     //this.head refers to any existing arg of contructor, if there are any
     const node = new Node(data, this.head)
+    //this makes sure it points to new node. by assigning new node to this.head, it is now the head, as its the newest node
     this.head = node
   }
 
@@ -30,6 +31,48 @@ class LinkedList {
     }
     return counter
   }
+
+  getFirst() {
+    // head property always points to first node of linked list
+    let firstNode = this.head
+    return firstNode
+  }
+
+  getLast() {
+    let node = this.head
+    //if no node in list, get out
+    if (!node) {
+      return null
+    }
+
+    // while node exists
+    while (node) {
+      // if next node is null, return because its the last
+      if (!node.next) {
+        return node
+      }
+      //otherwise keep iterating through
+      node = node.next
+    }
+  }
+
+  clear() {
+    // head is the what keeps track, so zero it out
+    this.head = null
+  }
+
+  removeFirst() {
+    let node = this.head
+
+    // if no nodes in list return
+    if (!node) {
+      return null
+    }
+    // to remove first, point head to second node
+    // let node = new Node(data, this.head)
+    this.head = node.next
+  }
+
 }
 
 module.exports = { Node, LinkedList };
