e8e4a84509814b97915052a417ee4fa22a15d1b2
eternale88 <ethanrobbins@ymail.com>
2020-12-08 21:02:33 -0500

    find circular ll

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,25 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+
+//circular linkedlist, has not tail node, as it's pointing to an earlier node
+function circular(list) {
+  let slow = list.getFirst()
+  let fast = list.getFirst()
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next
+    fast = fast.next.next
+    //both pointing to same value means they are circular, don't do slow.next or fast.next equal
+    //because values of multiple nodes may be same
+    //so we check whether they are the same object in memory
+    if (slow === fast) {
+      return true
+    }
+  }
+  //if we ever exit loop mean it's not circular, as it reached the end, so return false
+  return false
+
+}
 
 module.exports = circular;
