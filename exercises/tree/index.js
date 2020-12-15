1ec6994282f5f6df5bdc160801fdd41669c91b89
eternale88 <ethanrobbins@ymail.com>
2020-12-14 20:41:40 -0500

    node class for tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,27 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data
+    this.children = []
+  }
 
-class Tree {}
+  add(data) {
+    // const node = new Node(data)
+    this.children.push(new Node(data))
+  }
+
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      //!== because filter, only removes for false values, so all that not equal data are removed
+      return node.data !== data
+    })
+
+
+  }
+}
+
+class Tree { }
 
 module.exports = { Tree, Node };
