340cbaa7adf36b0589c8f4b498697ef2274866da
eternale88 <ethanrobbins@ymail.com>
2020-12-28 20:35:10 -0500

    level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, null]
+  const counters = [0]
+
+  //we always add null, so array must be greater than 1 to be looped through, or would have infinite loop
+  while (arr.length > 1) {
+    const node = arr.shift()
+
+    //if see this null value, we have reached end of that level, so we take it back and put at very end of our array, and add 0 onto counters arr, to show we are moving onto
+    //next row for our counter
+    if (node === null) {
+      arr.push(null)
+      counters.push(0)
+    } else {
+      //else meaning our node has a value
+      arr.push(...node.children)
+      //increments last el of array
+      counters[counters.length - 1]++
+
+    }
+  }
+  return counters
+}
 
 module.exports = levelWidth;
