14e99ef25b263e19615289a1e15c115589aa1852
eternale88 <ethanrobbins@ymail.com>
2021-01-04 20:47:53 -0500

    validate bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,26 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // handles when max bound not set yet, and current node is out of bounds of that max value, then something is wrong with the tree
+  if (max !== null && node.data > max) {
+    return false
+  }
+
+  //same but for left side
+  if (min !== null && node.data < min) {
+    return false
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false
+  }
+
+  return true
+}
 
 module.exports = validate;
