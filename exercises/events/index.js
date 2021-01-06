8994f093181a633d7ab25e3881a775a86a610eb4
eternale88 <ethanrobbins@ymail.com>
2021-01-06 15:44:37 -0500

    eventing library

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,39 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+
+  constructor() {
+    this.events = {}
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    //add as key to events obj
+    //if key exists already, we would already have array that we added, so we can push callback onto that array
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback)
+    } else {
+      //else create array, and have it contain our initial callback
+      this.events[eventName] = [callback]
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    //if any event of the trigger name on the array
+    //then we loop through and call the ones with that event type
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb()
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName]
+  }
 }
 
 module.exports = Events;
