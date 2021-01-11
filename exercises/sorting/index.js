68e5e42dbf3c0b5458916763c36892a0bb4074ec
eternale88 <ethanrobbins@ymail.com>
2021-01-10 20:30:51 -0500

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,8 +1,21 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
-
+// sorting is usually taking and array of numbers and sorting them from least to greatest
+//bubblesort is finding largest number, and dragging to the end of the array
 function bubbleSort(arr) {
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i -1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1]
+        //move larger one to right
+        arr[j + 1] = arr[j]
+        //moves lesser one to left
+        arr[j] = lesser
+      }
+    }
+  }
+  return arr
 }
 
 function selectionSort(arr) {
